9b330d794a1ce36baae4f9467ca6248580a84df2
farhaghallab3 <farhagalab74@gmail.com>
2024-12-31 17:05:05 +0200

    update ci/cd

--- frontend/cypress/support/e2e.js
+++
@@ -1,8 +0,0 @@
-Cypress.Commands.add("login", (username, password) => {
-    cy.visit("/");
-    cy.get('input[placeholder="Enter name"]').type(username);
-    cy.get('input[placeholder="Enter password"]').type(password);
-    cy.get("button").contains("Login").click();
-    cy.url().should("include", "/home");
-  });
-  
